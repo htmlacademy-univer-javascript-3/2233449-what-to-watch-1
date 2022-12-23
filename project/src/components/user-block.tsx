9ffc30cdbfc259302fd138093ae57d224e436ed1
Shrein Olga <olja_sr@mail.ru>
2022-12-23 14:01:47 +0500

    my-list

--- project/src/components/user-block.tsx
+++ project/src/components/user-block.tsx
@@ -1,11 +1,12 @@
 import {useAppDispatch, useAppSelector} from '../hooks';
 import {logoutAction} from '../api-action';
 import {AuthorizationStatus, LOGIN_ROUT} from '../constants';
-import {Link} from 'react-router-dom';
+import {Link, useNavigate} from 'react-router-dom';
 import {getAuthorizationStatus, getUser} from '../store/user-reducer/selector';
 
 function UserBlock() {
   const dispatch = useAppDispatch();
+  const navigate = useNavigate();
   const authorizationStatus = useAppSelector(getAuthorizationStatus);
   const user = useAppSelector(getUser);
   return (
@@ -13,7 +14,7 @@ function UserBlock() {
       {authorizationStatus === AuthorizationStatus.Auth ?
         <>
           <li className="user-block__item">
-            <div className="user-block__avatar">
+            <div className="user-block__avatar" onClick={() => navigate('/mylist')}>
               <img src={user?.avatarUrl} alt="User avatar" width="63" height="63"/>
             </div>
           </li>
